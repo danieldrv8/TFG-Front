848d31c4f15cf3800878b4b17827f9fa6976f11d
Daniel <danieldrv8@gmail.com>
2024-06-06 21:59:40 +0200

    main y header casi terminados

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -11,6 +11,7 @@ export class MainComponent implements OnInit {
 
   public selectedRoom?: string;
   sitesShown: Site[] = [];
+  imageShown: string = '';
 
   rooms: Room[] = [
     {
@@ -18,31 +19,57 @@ export class MainComponent implements OnInit {
         { id: 1, occupied: false },
         { id: 2, occupied: true },
         { id: 3, occupied: false },
-        { id: 4, occupied: false }
+        { id: 4, occupied: false },
+        { id: 5, occupied: false },
+        { id: 6, occupied: true },
+        { id: 7, occupied: false },
+        { id: 8, occupied: true },
+        { id: 9, occupied: false },
+        { id: 10, occupied: true },
+        { id: 11, occupied: true },
+        { id: 12, occupied: false },
+        { id: 13, occupied: true },
+        { id: 14, occupied: false },
       ],
       temp: 20,
       name: 'Sala 1',
-      image: ''
+      image: '../../../assets/img/plano-biblioteca2.png'
     },
     {
       sites: [
         { id: 1, occupied: true },
         { id: 2, occupied: false },
         { id: 3, occupied: true },
-        { id: 4, occupied: true }
+        { id: 4, occupied: true },
+        { id: 5, occupied: false },
+        { id: 6, occupied: false },
+        { id: 7, occupied: false },
+        { id: 8, occupied: true },
+        { id: 9, occupied: false },
+        { id: 10, occupied: true },
+        { id: 11, occupied: false },
+        { id: 12, occupied: false },
+        { id: 13, occupied: true },
+        { id: 14, occupied: true },
       ],
       temp: 25,
       name: 'Sala 2',
-      image: ''
+      image: '../../../assets/img/plano-biblioteca2.png'
     }
   ];
 
   ngOnInit(): void {
     this.updateSitesShown();
+    this.updateImageShown();
   }
 
   updateSitesShown(): void {
     this.sitesShown = this.rooms.find(room => room.name === this.selectedRoom)?.sites || [];
+    this.updateImageShown();
+  }
+
+  updateImageShown(): void {
+    this.imageShown = this.rooms.find(room => room.name === this.selectedRoom)?.image || '';
   }
 
 }
